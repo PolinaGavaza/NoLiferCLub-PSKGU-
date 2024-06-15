0ed681a0a0fa613f8d379ca74b6937616531c332
Polina <gavaza@endesign.ru>
2024-06-15 16:03:58 +0300

    Гаваза П.В. Создание классов, скрипта для подключения БД

---
+++ NoLiferCLub/Data/DataBaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+
+public class DatabaseConnection
+{
+    private readonly string _connectionString;
+
+    public DatabaseConnection(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public NpgsqlConnection GetConnection()
+    {
+        return new NpgsqlConnection(_connectionString);
+    }
+}
